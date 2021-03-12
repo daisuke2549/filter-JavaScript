e91230e6a9791d392c3946a93e609167f140d5c4
daisuke sasaki <gerund25495641@gmail.com>
2021-03-12 11:53:31 +0900

    ALLの部分を除いた動作確認済み段階でのコード

--- js/main.js
+++ js/main.js
@@ -1,18 +1,19 @@
 $(function(){
-	var box = $('.js_target');//検索対象のDOMを格納する
+	var box = $('.js_target');//検索対象
 	var conditions = $('.js_conditions');//現在の条件の選択状況を保持するオブジェクト
 	var findConditions;//各data-typeの子要素(input)を格納する
 	var currentType;//現在のdata-typeを示す
 	var count = 0;//検索ヒット数
-	var checkcount = 0;//各data-typeのチェックボックス選択数
-	var data_check = 0;//対象項目のデータがどれだけチェック状態と一致しているか
+	var checkcount = 0;//各data-typeのチェックボックス選択数 初期値は0
+	var data_check = 0;//対象項目のデータがどれだけチェック状態と一致しているか。初期値は0
    
 	var condition ={};//チェックボックスの入力状態を保持するオブジェクト
    
    
 	for(var i = 0; i < conditions.length; i++){//文字列の長さや配列の要素数などを取得するためのプロパティlengthを使用
-	  currentType = conditions[i].getAttribute('data-type');　//getAttributeでdata-typeを取得  current typeは現在のdata-type(term shikakuなど)
-	  condition[currentType] = [];
+	  currentType = conditions[i].getAttribute('data-type');　//getAttributeで対象のdata-typeを取得  current typeは現在のdata-type(term shikakuなど)
+	  condition[currentType] = [];  //対象のデータタイプを配列で渡す
+	//   console.log([currentType]);
 	}
    
 	function setConditions(){//条件設定
@@ -24,7 +25,9 @@ $(function(){
    
 		currentType = conditions[i].getAttribute('data-type');
    
-		findConditions = conditions[i].querySelectorAll('input');  //inputタグの要素を取得
+		findConditions = conditions[i].querySelectorAll('input');  //inputタグの要素data-type毎に取得
+
+		// console.log(findConditions)
    
 		for(var n = 0; n< findConditions.length; n++){//inputごとの処理
    
@@ -34,9 +37,9 @@ $(function(){
 		  } else {
 			condition[currentType][findConditions[n].value] = false;
 		  }
-		  if(findConditions.length === n+1){// inputの要素の個数が　チェックされている数＋1(つまり最後の要素の場合)の処理
+		  if(findConditions.length === n+1){//ループが最後のときの処理
 			if(checkcount === 0){
-			  for(var t = 0; t < findConditions.length; t++){
+			  for(var t = 0; t < findConditions.length; t++){    
 				condition[currentType][findConditions[t].value] = true;
 			  }
 			}
@@ -46,17 +49,16 @@ $(function(){
 	  }
    
    
-	  for(var m = 0, len = box.length; m< len; ++m){//最初に取得したターゲットの情報と、現在のinputの選択状態を比較して処理を行う
+	  for(var m = 0, len = box.length; m< len; ++m){//最初に取得した検索情報と、現在のinputの選択状態を比較して処理を行う
    
 		for(var i = 0; i < conditions.length; i++){//ターゲットのdata-typeを参照し、メソッドとしてconditionに個別に代入する
-		  currentType = conditions[i].getAttribute('data-type');
-		  //現在のターゲットのtype情報をカンマ区切りで分割し、配列に代入
+		  currentType = conditions[i].getAttribute('data-type');//data-typeごとに要素を取得
 		  var currentBoxTypes = $(box[m]).data(currentType).split(',');
   
 		  for(var j = 0; j < currentBoxTypes.length; j++){
 			if(condition[currentType][currentBoxTypes[j]]){
 			  data_check++;//選択した条件のうちひとつでもマッチしてたらdata_checkを加算してループを抜ける
-			  break;
+			  break
 			} else {
 	 
 			}
@@ -86,8 +88,4 @@ $(function(){
    
 
   });
-
-
-
-
 
